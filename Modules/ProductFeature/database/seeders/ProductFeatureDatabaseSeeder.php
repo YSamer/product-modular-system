7f4babc24ef79e186eb823494a961165314c3f43
YSamer <ysamer2525@gmail.com>
2025-02-15 06:38:45 +0200

    first commit

---
+++ Modules/ProductFeature/database/seeders/ProductFeatureDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\ProductFeature\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ProductFeatureDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/ProductFeature/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('productfeature::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('productfeature.name') !!}</p>
+@endsection
